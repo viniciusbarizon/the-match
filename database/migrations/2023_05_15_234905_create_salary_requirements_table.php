7b4471e9c4eb61783cfa4e531c1ecaac4d98b316
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2023-05-16 00:58:23 +0100

    create_salary_requirements_table

---
+++ database/migrations/2023_05_15_234905_create_salary_requirements_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('salary_requirements', function (Blueprint $table) {
+            $table->ulid('id');
+            $table->primary('id');
+
+            $table->mediumInteger('amount')->unsigned();
+
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+            $table->softDeletes();
+
+            $table->foreignUlid('contract_id')->constrained();
+            $table->foreignUlid('currency_id')->constrained();
+            $table->foreignUlid('job_seeker_id')->constrained();
+
+            $table->unique(['contract_id', 'currency_id', 'job_seeker_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('salary_requirements');
+    }
+};
