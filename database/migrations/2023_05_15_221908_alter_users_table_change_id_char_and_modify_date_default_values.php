b733d691d09e4c7feb26e7673d6f6f223d895c6a
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2023-05-15 23:46:12 +0100

    migration: alter_users_table_change_id_char_and_modify_date_default_values

---
+++ database/migrations/2023_05_15_221908_alter_users_table_change_id_char_and_modify_date_default_values.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->char('id', 26)->change();
+
+            $table->string('name')
+                ->after('email_verified_at')
+                ->change();
+
+            $table->timestamp('created_at')
+                ->useCurrent()
+                ->nullable(false)
+                ->change();
+
+            $table->timestamp('updated_at')
+                ->useCurrent()
+                ->nullable(false)
+                ->change();
+
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->bigInteger('id')
+                ->autoIncrements()
+                ->change();
+
+            $table->string('name')
+                ->after('id')
+                ->change();
+
+            $table->timestamp('created_at')
+                ->default(null)
+                ->nullable()
+                ->change();
+
+            $table->timestamp('updated_at')
+                ->default(null)
+                ->nullable()
+                ->change();
+        });
+    }
+};
