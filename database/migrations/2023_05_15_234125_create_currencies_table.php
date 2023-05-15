16821057c328b0374e1702759c8c7587cc8e8c4d
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2023-05-16 00:47:58 +0100

    create_currencies_table

---
+++ database/migrations/2023_05_15_234125_create_currencies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->ulid('id');
+            $table->primary('id');
+
+            $table->string('name', 20)->unique();
+            $table->enum('salary_requirement_per', ['month', 'year']);
+            $table->char('symbol', 2);
+
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('currencies');
+    }
+};
