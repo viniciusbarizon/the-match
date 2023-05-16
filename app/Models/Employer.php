486895fc329efcda6c7b57c86478572e9069789c
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2023-05-16 23:34:28 +0100

    model Employer

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Employer extends Model
 {
     use HasFactory;
+    use HasUlids;
+
+    protected $guarded = [];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
