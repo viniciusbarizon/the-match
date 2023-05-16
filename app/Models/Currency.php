132abdb01ee99f852b9315733457eebdae16a664
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2023-05-16 23:44:34 +0100

    model Currency

--- app/Models/Currency.php
+++ app/Models/Currency.php
@@ -5,17 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Employer extends Model
+class Currency extends Model
 {
     use HasFactory;
     use HasUlids;
 
-    protected $guarded = [];
-
-    public function user(): BelongsTo
-    {
-        return $this->belongsTo(User::class);
-    }
+    protected $fillable = [];
 }
