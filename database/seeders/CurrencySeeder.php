190526bd97d505d940b6742deb6867b2530b4a9e
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2023-05-16 22:44:59 +0100

    CurrencySeeder

--- database/seeders/CurrencySeeder.php
+++ database/seeders/CurrencySeeder.php
@@ -17,7 +17,7 @@ class CurrencySeeder extends Seeder
             'id' => '01H0K88685BR21KWWR72ARQDJK',
             'name' => 'Brazilian real',
             'salary_requirement_per' => 'month',
-            'symbol' => 'R$'
+            'symbol' => 'R$',
         ]);
     }
 }
