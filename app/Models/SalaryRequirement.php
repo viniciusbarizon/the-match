fcbe73d6242e4341598cb52a3424adf4624f78aa
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2023-05-16 23:49:39 +0100

    model SalaryRequirement

---
+++ app/Models/SalaryRequirement.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class SalaryRequirement extends Model
+{
+    use HasFactory;
+    use HasUlids;
+
+    protected $fillable = [
+        'amount',
+        'contract_id',
+        'currency_id',
+        'job_seeker_id',
+    ];
+
+    public function contract(): BelongsTo
+    {
+        return $this->belongsTo(Contract::class);
+    }
+
+    public function currency(): BelongsTo
+    {
+        return $this->belongsTo(Currency::class);
+    }
+
+    public function jobSeeker(): BelongsTo
+    {
+        return $this->belongsTo(JobSeeker::class);
+    }
+}
