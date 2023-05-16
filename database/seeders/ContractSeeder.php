08f8b6889606efd37c8a7d383bf88a17440111bc
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2023-05-16 22:34:40 +0100

    ContractSeeder

---
+++ database/seeders/ContractSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class ContractSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('contracts')->insert([
+            ['id' => '01H0K7HJTN82AYK1FRADW0P282', 'name' => 'Contractor'],
+            ['id' => '01H0K7HJTN82AYK1FRADW0P283', 'name' => 'Permanent employee']
+        ]);
+    }
+}
