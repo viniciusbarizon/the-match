07897cbaea1b4219ab43e9f3f325d72243dfdf1f
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2023-05-16 23:26:03 +0100

    JobSeeker belongsTo User

--- app/Models/JobSeeker.php
+++ app/Models/JobSeeker.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class JobSeeker extends Model
 {
@@ -15,4 +17,9 @@ class JobSeeker extends Model
         'slug',
         'user_id',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
