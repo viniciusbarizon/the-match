e529d4b3b7fa799575dfd5717d996e9821813512
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2023-05-18 22:30:11 +0100

    add soft deletes to models

--- app/Models/Contract.php
+++ app/Models/Contract.php
@@ -6,11 +6,13 @@ use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Contract extends Model
 {
     use HasFactory;
     use HasUlids;
+    use SoftDeletes;
 
     protected $fillable = [];
 
--- app/Models/JobSeeker.php
+++ app/Models/JobSeeker.php
@@ -7,11 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class JobSeeker extends Model
 {
     use HasFactory;
     use HasUlids;
+    use SoftDeletes;
 
     protected $fillable = [
         'slug',
