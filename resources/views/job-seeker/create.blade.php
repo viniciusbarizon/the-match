0f77b309c73b97d49cc438fa9e9ad9b3c7e63a18
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2023-05-21 15:20:15 +0100

    job seeker create

--- resources/views/job-seeker/create.blade.php
+++ resources/views/job-seeker/create.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div>
-        Preencha os dados abaixo, receba um link para compartilhar com as empresas, e saiba se o salário ofertado na
-        vaga é compatível com a sua pretensão salarial antes de iniciar o processo seletivo.
+        Preencha os dados abaixo, receba um link para compartilhar com as empresas, e saiba antes de iniciar o processo
+        seletivo se o salário ofertado na vaga é compatível com a sua pretensão salarial.
     </div>
 </x-guest-layout>
