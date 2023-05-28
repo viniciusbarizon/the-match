12255cfb6e770d67aebc5071645d6a267576b36e
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2023-05-28 18:12:10 +0100

    ContractSeeder remove contractor

--- database/seeders/ContractSeeder.php
+++ database/seeders/ContractSeeder.php
@@ -13,7 +13,6 @@ class ContractSeeder extends Seeder
     public function run(): void
     {
         DB::table('contracts')->insert([
-            ['id' => '01H0K7HJTN82AYK1FRADW0P282', 'name' => 'Contractor'],
             ['id' => '01H0K7HJTN82AYK1FRADW0P283', 'name' => 'Permanent employee'],
         ]);
     }
