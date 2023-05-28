115572a34295f3f4f0fdd91838600a7b15cb592c
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2023-05-28 18:14:38 +0100

    DatabaseSeeder fix contract duplicated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             ContractSeeder::class,
-            ContractSeeder::class,
+            CurrencySeeder::class,
         ]);
     }
 }
