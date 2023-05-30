0f3e91869b52e20498a1f66de618ec2aefa8b8c1
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2023-05-31 00:55:42 +0100

    url

--- resources/views/livewire/job-seeker/create-or-edit/url.blade.php
+++ resources/views/livewire/job-seeker/create-or-edit/url.blade.php
@@ -9,13 +9,11 @@
     <div>
         <x-input-label for="name" :value="__('URL')" />
 
-        <div class="flex flex-row mt-1 space-x-1 items-center">
-            <div class="h-6 text-sm">
-                https://thematch.dev/with/
-            </div>
-
-            <x-text-input class="grow" dusk="slug" maxlength="255" name="slug" type="text" :value="old('slug')"
-                wire:model.defer="slug" required/>
+        <div class="h-5 text-sm">
+            https://thematch.dev/with/
         </div>
+
+        <x-text-input class="w-full" dusk="slug" maxlength="255" name="slug" type="text" :value="old('slug')"
+            wire:model.defer="slug" required/>
     </div>
 </div>
