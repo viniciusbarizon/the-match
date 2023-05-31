8d875276d187800a61b7090e14362cd84e5fa292
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2023-05-31 18:38:39 +0100

    url

--- app/Http/Livewire/JobSeeker/CreateOrEdit/Url.php
+++ app/Http/Livewire/JobSeeker/CreateOrEdit/Url.php
@@ -8,6 +8,8 @@ class Url extends Component
 {
     public string $inputName = 'name';
 
+    public string $inputSlug = 'slug';
+
     public function render()
     {
         return view('livewire.job-seeker.create-or-edit.url');
--- resources/views/livewire/job-seeker/create-or-edit/url.blade.php
+++ resources/views/livewire/job-seeker/create-or-edit/url.blade.php
@@ -7,13 +7,13 @@
     </div>
 
     <div>
-        <x-input-label for="slug" :value="__('URL')" />
+        <x-input-label for="{{ $inputSlug }}" :value="__('URL')" />
 
         <div class="h-5 text-sm">
             https://thematch.dev/with/
         </div>
 
-        <x-text-input class="w-full" dusk="slug" maxlength="255" name="slug" type="text" :value="old('slug')"
-            wire:model.defer="slug" required/>
+        <x-text-input class="w-full" dusk="{{ $inputSlug }}" maxlength="255" name="{{ $inputSlug }}" type="text"
+            :value="old($inputSlug)" wire:model.defer="{{ $inputSlug }}" required/>
     </div>
 </div>
