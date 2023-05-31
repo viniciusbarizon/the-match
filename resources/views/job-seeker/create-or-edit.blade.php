7a8b77d97e94aa3d18966c351f7cf1ffd49e69bc
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2023-05-31 18:27:48 +0100

    translate text

--- resources/views/job-seeker/create-or-edit.blade.php
+++ resources/views/job-seeker/create-or-edit.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <div class="mb-4">
-        Preencha os dados abaixo, receba um link para compartilhar com as empresas, e saiba antes de iniciar o processo
-        seletivo se o salário ofertado é compatível com a sua pretensão salarial.
+        {{ __('Preencha os dados abaixo, receba um link para compartilhar com as empresas, e saiba antes de iniciar o
+            processo seletivo se o salário ofertado é compatível com a sua pretensão salarial.') }}
     </div>
 
     @livewire('job-seeker.verification-code.send')
