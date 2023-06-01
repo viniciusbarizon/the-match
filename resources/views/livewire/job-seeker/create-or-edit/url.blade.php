1ece74a00da0263e4e72ec739d865f1059b1c05f
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2023-06-01 21:30:03 +0100

    URL input ids

--- resources/views/livewire/job-seeker/create-or-edit/url.blade.php
+++ resources/views/livewire/job-seeker/create-or-edit/url.blade.php
@@ -2,21 +2,23 @@
     <div>
         <x-input-label for="{{ $inputName }}" :value="__('Nome')" />
 
-        <x-text-input autocomplete="name" class="mt-1 w-full" dusk="{{ $inputName }}" maxlength="255"
-            name="{{ $inputName }}" type="text" :value="old($inputName)" wire:model.delay="{{ $inputName }}"
-            required />
+        <x-text-input autocomplete="name" class="mt-1 w-full" dusk="{{ $inputName }}" id="{{ $inputName }}"
+            maxlength="255" name="{{ $inputName }}" type="text" :value="old($inputName)"
+            wire:model.delay="{{ $inputName }}" required />
     </div>
 
     <div>
         <x-input-label for="{{ $inputSlug }}" :value="__('Slug')" />
 
-        <x-text-input class="mt-1 w-full" dusk="{{ $inputSlug }}" maxlength="255" name="{{ $inputSlug }}" type="text"
-            :value="old($inputSlug)" wire:model.delay="{{ $inputSlug }}" required/>
+        <x-text-input class="mt-1 w-full" dusk="{{ $inputSlug }}" id="{{ $inputSlug }}" maxlength="255"
+            name="{{ $inputSlug }}" type="text" :value="old($inputSlug)" wire:model.delay="{{ $inputSlug }}"
+            required />
     </div>
 
     <div>
         <x-input-label for="{{ $inputUrl }}" :value="__('URL')" />
 
-        <x-text-input class="mt-1 w-full" dusk="{{ $inputUrl }}" type="text" wire:model.defer="{{ $inputUrl }}" readonly />
+        <x-text-input class="mt-1 w-full" dusk="{{ $inputUrl }}" id="{{ $inputUrl }}" name="{{ $inputUrl }}"
+            type="text" wire:model.defer="{{ $inputUrl }}" readonly />
     </div>
 </div>
