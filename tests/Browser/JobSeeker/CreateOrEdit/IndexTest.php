0c810d8dad627df9456f8582a2c9da07eb0f16f0
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2023-06-04 18:26:38 +0100

    dusk currency

--- tests/Browser/JobSeeker/CreateOrEdit/IndexTest.php
+++ tests/Browser/JobSeeker/CreateOrEdit/IndexTest.php
@@ -9,7 +9,11 @@ class IndexTest extends DuskTestCase
 {
     const CONTRACT = 'contract_id';
 
+    const CURRENCY = 'currency_id';
+
     const DUSK_CONTRACT = '@contract_id';
+
+    const DUSK_CURRENCY = '@currency_id';
 
     const LOGO = '@logo';
 
@@ -28,7 +32,12 @@ class IndexTest extends DuskTestCase
                 ->assertAttribute(self::DUSK_CONTRACT, 'id', self::CONTRACT)
                 ->assertAttribute(self::DUSK_CONTRACT, 'name', self::CONTRACT)
                 ->assertAttribute(self::DUSK_CONTRACT, 'required', true)
-                ->assertSelected(self::DUSK_CONTRACT, '01H0K7HJTN82AYK1FRADW0P283');
+                ->assertSelected(self::DUSK_CONTRACT, '01H0K7HJTN82AYK1FRADW0P283')
+                ->assertVisible(self::DUSK_CURRENCY)
+                ->assertAttribute(self::DUSK_CURRENCY, 'id', self::CURRENCY)
+                ->assertAttribute(self::DUSK_CURRENCY, 'name', self::CURRENCY)
+                ->assertAttribute(self::DUSK_CURRENCY, 'required', true)
+                ->assertSelected(self::DUSK_CURRENCY, '01H0K88685BR21KWWR72ARQDJK');
         });
     }
 }
