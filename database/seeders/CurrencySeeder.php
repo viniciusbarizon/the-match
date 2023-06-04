5b24ff73918e9c4810781c5f6d657b4d5fbfecfe
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2023-06-04 18:12:55 +0100

    currency

--- database/seeders/CurrencySeeder.php
+++ database/seeders/CurrencySeeder.php
@@ -14,7 +14,7 @@ class CurrencySeeder extends Seeder
     {
         DB::table('currencies')->insert([
             'id' => '01H0K88685BR21KWWR72ARQDJK',
-            'name' => 'Brazilian real',
+            'name' => 'Real',
             'salary_requirement_per' => 'month',
             'symbol' => 'R$',
         ]);
