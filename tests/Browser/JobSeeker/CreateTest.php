851153f962f5b9eb4e28198c86d373af386f5d7f
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2023-06-10 18:02:42 +0100

    dusk remove truncation

--- tests/Browser/JobSeeker/CreateTest.php
+++ tests/Browser/JobSeeker/CreateTest.php
@@ -2,17 +2,12 @@
 
 namespace Tests\Browser\JobSeeker;
 
-use Illuminate\Foundation\Testing\DatabaseTruncation;
 use Laravel\Dusk\Browser;
 use Tests\Browser\Pages\JobSeeker\Create;
 use Tests\DuskTestCase;
 
 class CreateTest extends DuskTestCase
 {
-    use DatabaseTruncation;
-
-    protected $tablesToTruncate = ['job_seekers'];
-
     public function testCreate(): void
     {
         $this->browse(function (Browser $browser) {
