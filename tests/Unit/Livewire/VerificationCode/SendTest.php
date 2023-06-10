31f67d4b3bd16fdd6bce2016dcae1b907522e90c
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2023-06-10 19:08:06 +0100

    test send verification code

--- tests/Unit/Livewire/VerificationCode/SendTest.php
+++ tests/Unit/Livewire/VerificationCode/SendTest.php
@@ -1,22 +1,29 @@
 <?php
 
 use App\Http\Livewire\VerificationCode\Send;
+use App\Models\JobSeeker;
 use NextApps\VerificationCode\Models\VerificationCode;
 use function Pest\Livewire\livewire;
 
-it('can be validated with required email', function () {
+it('validates an email as required', function () {
     livewire(Send::class)
         ->call('send')
         ->assertHasErrors(['email' => 'required']);
 });
 
-it('can be validated with invalid email', function () {
+it('validates an invalid email', function () {
     livewire(Send::class, ['email' => str()->random(25)])
         ->call('send')
         ->assertHasErrors(['email' => 'email']);
 });
 
-it('can be sent', function () {
+it('validates email as already in use', function () {
+    livewire(Send::class, ['email' => JobSeeker::factory()->create()->email])
+        ->call('send')
+        ->assertHasErrors(['email' => 'unique']);
+});
+
+it('sends', function () {
     $now = strtotime('now');
 
     livewire(Send::class, ['email' => fake()->email()])
