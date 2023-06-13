44815b4ce91cab7e815f4807ab16e4d34cbf8a43
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2023-06-13 23:25:31 +0100

    job seeker store

--- resources/views/job-seeker/store.blade.php
+++ resources/views/job-seeker/store.blade.php
@@ -1,19 +1,3 @@
 <x-guest-layout>
-    <div class="space-y-8">
-        <p class="font-bold">
-            {{ __('Seu perfil foi criado com sucesso!') }}
-        </p>
-
-        <p>
-            {{ __('Guarde o link para compartilhar com as empresas:') }}
-
-            <a href="{{ route('job-seekers.match', ['slug' => $slug]) }}" target="_blank">
-                {{ route('job-seekers.match', ['slug' => $slug]) }}
-            </a>
-        </p>
-
-        <p>
-            {{ __('Enviaremos para o seu e-mail os matches feitos pelas empresas.') }}
-        </p>
-    </div>
+    <x-job-seeker.store slug="{{ $slug }}" />
 </x-guest-layout>
