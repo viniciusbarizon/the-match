1f90afca4db56e6b4db31bf70745114fc0597705
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2023-06-27 19:55:10 +0100

    white button border

--- app/Http/Livewire/JobSeeker/Create/Code/Verify.php
+++ app/Http/Livewire/JobSeeker/Create/Code/Verify.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Livewire\JobSeeker\Create\Code;
 
-use App\Http\Requests\JobSeeker\VerificationCode\VerifyRequest;
+use App\Http\Requests\JobSeeker\Code\VerifyRequest;
 use Illuminate\View\View;
 use Livewire\Component;
 use NextApps\VerificationCode\Models\VerificationCode as VerificationCodeModel;
