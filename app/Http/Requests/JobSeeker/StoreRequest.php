8c577897020d61fa023ff53fd5d5a15d8f07733d
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2023-07-01 15:35:02 +0100

    store request regex url

--- app/Http/Requests/JobSeeker/StoreRequest.php
+++ app/Http/Requests/JobSeeker/StoreRequest.php
@@ -26,14 +26,16 @@ class StoreRequest extends FormRequest
      */
     public function rules(): array
     {
+        $url = route('job-seekers.match', ['slug' => $this->slug]);
+
         return [
-            'contract_id' => ['bail', 'required', 'ulid', 'exists:contracts,id'],
-            'currency_id' => ['bail', 'required', 'ulid', 'exists:currencies,id'],
-            'email' => ['bail', 'required', 'max:255', 'email', 'unique:job_seekers', new EmailVerify, new EmailEqualsSession],
-            'name' => ['bail', 'required', 'string', 'max:255'],
-            'salary' => ['bail', 'required', 'integer', 'between:1,16777215'],
-            'slug' => ['bail', 'required', 'string', 'max:255', 'lowercase'],
-            'url' => ['bail', 'required', 'string', 'max:255', 'lowercase', 'url'],
+            'contract_id' => ["bail", "required", "ulid", "exists:contracts,id"],
+            'currency_id' => ["bail", "required", "ulid", "exists:currencies,id"],
+            'email' => ["bail", "required", "max:255", "email", "unique:job_seekers", new EmailVerify, new EmailEqualsSession],
+            'name' => ["bail", "required", "string", "max:255"],
+            'salary' => ["bail", "required", "integer", "between:1,16777215"],
+            'slug' => ["bail", "required", "string", "max:255", "lowercase"],
+            'url' => ["bail", "required", "string", "max:255", "lowercase", "url", "regex:($url)"],
         ];
     }
 }
