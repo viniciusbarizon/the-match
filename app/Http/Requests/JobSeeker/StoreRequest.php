a9fd5856842bec7d0b90a12564206b4e39151e81
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2023-07-01 15:35:53 +0100

    store request order

--- app/Http/Requests/JobSeeker/StoreRequest.php
+++ app/Http/Requests/JobSeeker/StoreRequest.php
@@ -29,13 +29,13 @@ class StoreRequest extends FormRequest
         $url = route('job-seekers.match', ['slug' => $this->slug]);
 
         return [
-            'contract_id' => ["bail", "required", "ulid", "exists:contracts,id"],
-            'currency_id' => ["bail", "required", "ulid", "exists:currencies,id"],
             'email' => ["bail", "required", "max:255", "email", "unique:job_seekers", new EmailVerify, new EmailEqualsSession],
             'name' => ["bail", "required", "string", "max:255"],
-            'salary' => ["bail", "required", "integer", "between:1,16777215"],
             'slug' => ["bail", "required", "string", "max:255", "lowercase"],
             'url' => ["bail", "required", "string", "max:255", "lowercase", "url", "regex:($url)"],
+            'contract_id' => ["bail", "required", "ulid", "exists:contracts,id"],
+            'currency_id' => ["bail", "required", "ulid", "exists:currencies,id"],
+            'salary' => ["bail", "required", "integer", "between:1,16777215"],
         ];
     }
 }
