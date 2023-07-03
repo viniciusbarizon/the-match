b7fa70c2a8eb69653adc55d4dacc8bc2deb0070a
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2023-07-03 21:48:33 +0100

    mail job seeker stored

--- resources/views/emails/job-seekers/stored.blade.php
+++ resources/views/emails/job-seekers/stored.blade.php
@@ -1,3 +1,13 @@
 <div>
-    Vinicius, obrigado por criar um perfil no the match.
+    <p>
+        Vinicius, {{ __('obrigado por criar um perfil no the match.') }}
+    <p>
+
+    <p>
+        {{ __('Guarde o link para compartilhar com as empresas:') }}
+    </p>
+
+    <a href="http://localhost/with/vinicius" target="_blank">
+        http://localhost/with/vinicius
+    </a>
 </div>
