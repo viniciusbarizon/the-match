374409aca8b0c346b8597e9f7535f94d3a0d7d0c
Vinicius Barizon <vinicius.barizon@managingtheintelligence.pt>
2023-07-15 17:17:26 +0100

    StoreAction

--- app/Actions/JobSeeker/StoreAction.php
+++ app/Actions/JobSeeker/StoreAction.php
@@ -3,28 +3,47 @@
 namespace App\Actions;
 
 use App\Models\JobSeeker;
+use App\Models\SalaryRequirement;
 
 final class StoreAction
 {
+    private JobSeeker $jobSeeker;
+
     public function __construct(
         public readonly string $contractId,
         public readonly string $currencyId,
         public readonly string $email,
         public readonly string $name,
+        public readonly string $salary,
         public readonly string $slug,
     ) { }
 
     public function store(): void
     {
-
+        $this->createJobSeeker();
     }
 
-    private function create(): void
+    private function createJobSeeker(): void
     {
-        JobSeeker::create([
+        $this->jobSeeker = JobSeeker::create([
             'email' => $this->email,
             'name' => $this->name,
             'slug' => $this->slug,
-        ])
+        ]);
+    }
+
+    private function createSalaryRequirements(): void
+    {
+        $this->jobSeeker->salaryRequirements()
+            ->save($this->getSalaryRequirement());
+    }
+
+    private function getSalaryRequirement(): SalaryRequirement
+    {
+        return new SalaryRequirement([
+            'contract_id' => $this->contract_id,
+            'currency_id' => $this->contract_id,
+            'salary' => $this->salary
+        ]);
     }
 }
